3bb08d30cffb1e25659d24761d1e3cb8e2835454
smil98 <seungmilee98@gmail.com>
2023-09-07 09:09:40 +0900

    searchBar update

--- javascript/index.js
+++ javascript/index.js
@@ -6,14 +6,12 @@ $(document).ready(function () {
     var isOpen = false;
 
     $("#toggle-search").click(function () {
-        if (isOpen == false) {
-            $("#searchArea").css("width", "60%");
-            $("#searchBar").fadeIn();
+        if (!isOpen) {
+            $("#searchArea").addClass("expanded");
             isOpen = true;
-        } else if (isOpen == true) {
-            $("#searchArea").css("width", "20%");
-            $("#searchBar").fadeOut();
+        } else {
+            $("#searchArea").removeClass("expanded");
             isOpen = false;
         }
-    })
-})
+    });
+});
